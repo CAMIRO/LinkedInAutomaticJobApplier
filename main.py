8058464971846796d3ea4e9ccf60e9089423086a
CAMIRO <juancamilosc@hotmail.com>
2022-06-20 13:30:58 -0500

    adding the list jobs first steps

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 import time
 
@@ -17,27 +20,49 @@ options =  webdriver.ChromeOptions()
 options.add_argument('--start-maximized')
 options.add_argument('--disable-extensions')
 
-driver_path = '/home/camiro/Documents/python/Selenium/chromedriver'
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-driver = webdriver.Chrome(driver_path, options=options)
 
 
 driver.set_window_position(2000, 0)
 driver.maximize_window()
-time.sleep(1)
+
 
 
 driver.get('https://www.linkedin.com/jobs/search/?f_E=4&f_WT=2&keywords=react%20developer&location=Bogota%2C%20D.C.')
 
 # Sign in section:
-sign_in_button = driver.find_element_by_link_text("Sign in")
+
+sign_in_button = driver.find_element(by=By.LINK_TEXT, value="Sign in")
 sign_in_button.click()
 
-email_field = driver.find_element_by_id("username")
+
+email_field = driver.find_element(by=By.ID, value="username")
 email_field.send_keys(EMAIL_ADDRESS)
 
-password_field = driver.find_element_by_id("password")
+password_field = driver.find_element(by=By.ID, value="password")
 password_field.send_keys(EMAIL_PASSWORD)
 password_field.send_keys(Keys.ENTER)
 # driver.quit()
+
+
+# list jobs
+
+# all_listings = driver.find_element(by=By.CSS_SELECTOR, value=".occludable-update")
+# all_listings=[]
+# all_listings = driver.find_element(By.CSS_SELECTOR, ".job-card-container--clickable")
+content=[]
+content = driver.find_element(By.CLASS_NAME, 'job-card-container')
+
+# print("🚀 ~ ", type(content))
+# print("🚀 ~ ", content)
+for e in content:
+    print("🚀 ~ ", e)
+
+
+
+# for listing in all_listings:
+#     print("called")
+#     listing.click()
+#     time.sleep(2)
 
