9f270ae46ad09454c410ef814d8aaab4fa7c61eb
CAMIRO <juancamilosc@hotmail.com>
2022-06-20 11:03:01 -0500

    adding the env enviroment and loging in, on linkelin

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 import time
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
+
+EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
+EMAIL_PASSWORD =  os.getenv('EMAIL_PASSWORD')
+
 
 # Opciones de navegación
 options =  webdriver.ChromeOptions()
@@ -12,11 +22,22 @@ driver_path = '/home/camiro/Documents/python/Selenium/chromedriver'
 driver = webdriver.Chrome(driver_path, options=options)
 
 
-# Iniciarla en la pantalla 2
 driver.set_window_position(2000, 0)
 driver.maximize_window()
 time.sleep(1)
 
-# Inicializamos el navegador
-driver.get('https://www.linkedin.com')
-driver.quit()
+
+driver.get('https://www.linkedin.com/jobs/search/?f_E=4&f_WT=2&keywords=react%20developer&location=Bogota%2C%20D.C.')
+
+# Sign in section:
+sign_in_button = driver.find_element_by_link_text("Sign in")
+sign_in_button.click()
+
+email_field = driver.find_element_by_id("username")
+email_field.send_keys(EMAIL_ADDRESS)
+
+password_field = driver.find_element_by_id("password")
+password_field.send_keys(EMAIL_PASSWORD)
+password_field.send_keys(Keys.ENTER)
+# driver.quit()
+
